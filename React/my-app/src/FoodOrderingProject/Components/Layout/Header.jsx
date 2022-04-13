8db937059a3efa4bb80ce2b11ef77d5d3b211990
manishrajput11 <manishrajputrajput123@gmail.com>
2022-04-13 21:06:05 +0530

    my -app

--- React/my-app/src/FoodOrderingProject/Components/Layout/Header.jsx
+++ React/my-app/src/FoodOrderingProject/Components/Layout/Header.jsx
@@ -1,19 +1,21 @@
-import React, { Fragment } from 'react'
+import { Fragment } from 'react';
 
-import HeaderCartButton from './HeaderCartButton'
-import Mealimage from '../../Image/meals.jpg'
-import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../Image/meals.jpg';
+import classes from './Header.module.css';
 
-const Header = props=> {
-    return <Fragment>
-        <header className={classes.header}>
-            <h1>FoodOrder App</h1>
-            <HeaderCartButton onClick={props.onShowCart} />
-        </header>
-        <div className={classes['main-image']}>
-            <img src={Mealimage} alt='Picture of Meals'/>
-        </div>
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
     </Fragment>
-}
+  );
+};
 
 export default Header;
--- React/my-app/src/FoodOrderingProject/Components/Meals/MealsSummary.jsx
+++ React/my-app/src/FoodOrderingProject/Components/Meals/MealsSummary.jsx
@@ -1,10 +1,10 @@
 import classes from './MealsSummary.module.css';
 
 const MealsSummary = () => {
-    return(
-        <section className={classes.summary}>
-            <h2>Delicious food delivered to you</h2>
-            <p>
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Delivered To You</h2>
+      <p>
         Choose your favorite meal from our broad selection of available meals
         and enjoy a delicious lunch or dinner at home.
       </p>
@@ -17,5 +17,3 @@ const MealsSummary = () => {
 };
 
 export default MealsSummary;
-
-        
