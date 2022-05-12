ed7ba3846a630289aabac18799274e1840b3ae2f
ManishRajput11 <101552024+ManishRajput11@users.noreply.github.com>
2022-05-12 10:12:29 +0530

    increment11-5-2022

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -1,35 +1,42 @@
 import  ReactDOM  from "react-dom";
-import Forprops from './Forprops'
+import './index.css'
+import { Provider } from "react-redux";
+
+
+// import Forprops from './Forprops'
 // import ReactForms from './ReactForms'
 // import Assignment1 from './Assignment1';
-import Forref from './Forref'
-import Context from "./Context";
-
- import './index.css'
- import Forportals from './Forportals'
- import Foruseeffect from './Foruseeffect'
- import Foruseeffect2 from './Foruseeffect2'
- import Stateassignment from './Stateassignment'
- import Stateassignment2 from './stateassignment2'
- import Stateassignment3 from '../src/first project/Componenets/stateassignment3'
- import Staaateassignment from './first project/Componenets/staaateassignment'
- import Maincomponent from './Maincomponent'
+// import Forref from './Forref'
+// import Context from "./Context";
+//  import Forportals from './Forportals'
+//  import Foruseeffect from './Foruseeffect'
+//  import Foruseeffect2 from './Foruseeffect2'
+//  import Stateassignment from './Stateassignment'
+//  import Stateassignment2 from './stateassignment2'
+//  import Stateassignment3 from '../src/first project/Componenets/stateassignment3'
+//  import Staaateassignment from './first project/Componenets/staaateassignment'
+//  import Maincomponent from './Maincomponent'
 // import App from './App';
 // import ForState from './ForState'
-import Appp from './FoodOrderingProject/Appp'
-import React from "react";
+// import Appp from './FoodOrderingProject/Appp'
+// import React from "react";
 // import ToCountNumberBackward from './18-04-2022/ToCountNumberBackward'
-import ToCountNumberForward from './18-04-2022/ToCountNumberForward'
-import MainFileForhooks from './18-04-2022/MainFileForhooks'
-import Form from "./21-04-2022/FormsProject/Form";
-import ForForms from "./21-04-2022/ForForms"
-import OnBlur from "./21-04-2022/ForForms"
-import Checkout from "./FoodOrderingProject/Checkout"
+// import ToCountNumberForward from './18-04-2022/ToCountNumberForward'
+// import MainFileForhooks from './18-04-2022/MainFileForhooks'
+// import Form from "./21-04-2022/FormsProject/Form";
+// import ForForms from "./21-04-2022/ForForms"
+// import OnBlur from "./21-04-2022/ForForms"
+// import Checkout from "./FoodOrderingProject/Checkout"
+import reduxstore from './redux6'
+import Counter from './redux5'
 
 
 
 
-
-ReactDOM.render( <Checkout/>, document.getElementById('root'));
+ReactDOM.render( 
+    <Provider store={reduxstore}>
+        <Counter/>
+    </Provider> 
+    , document.getElementById('root'));
     
             
---
+++ React/my-app/src/redux5.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import {useSelector,useDispatch} from 'react-redux'
+const Counter = () => {
+  const dispatch=useDispatch()
+  const counter=useSelector(state => state.counter)
+  const toggleCounterHandler = () => {
+    dispatch({type:'increment' })
+  };
+  const increment=()=>{
+    dispatch({type:"increment"})
+  }
+  const decrement=()=>{
+    dispatch({type:"decrement"})
+  }
+  const increase=()=>{
+    dispatch({type:'increase',amount: 10})
+  }
+  return (
+    <main>
+      <h1>Redux Counter</h1>
+      <div>{counter}</div>
+      <div>
+        <button onClick={increment}>increment</button>
+        <button onClick={decrement}>decrement</button>
+        <button onClick={increase}>increase by 10</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+export default Counter
